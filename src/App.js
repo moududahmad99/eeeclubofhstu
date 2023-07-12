3bc99355405710f3d34cc2e49822065cdf91d995
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-07-12 20:41:25 +0600

    Complete Full Project -> EEECLUBOFHSTU

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import MainNavbar from "./pages/MainNavbar";
 
 function App() {
-  return (
-    <div className="App">
-      <MainNavbar />
-    </div>
-  );
+    return (
+        <div className="App">
+            <MainNavbar />
+        </div>
+    );
 }
 
 export default App;
